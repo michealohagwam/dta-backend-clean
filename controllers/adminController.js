fa77973986edff286e636943a27c702a645f60ff
Ohagwam Micheal <michealohgwam@gmail.com>
2025-07-07 19:18:22 +0100

    Remove sensitive MongoDB URI debug log

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -9,10 +9,10 @@ const Upgrade = require('../models/Upgrade');
 const EmailLog = require('../models/EmailLog');
 
 // Configure Nodemailer
-console.log('EMAIL_HOST:', process.env.EMAIL_HOST);
-console.log('EMAIL_PORT:', process.env.EMAIL_PORT);
-console.log('EMAIL_USER:', process.env.EMAIL_USER);
-console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
+//console.log('EMAIL_HOST:', process.env.EMAIL_HOST);
+//console.log('EMAIL_PORT:', process.env.EMAIL_PORT);
+//console.log('EMAIL_USER:', process.env.EMAIL_USER);
+//console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
